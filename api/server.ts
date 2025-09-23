eff3f75a878711cf7e0b32386b67f933a3f6ad5f
LuanAssis01 <assisluan863@gmail.com>
2025-09-23 00:10:27 -0300

    feat(config): add connection on database

---
+++ api/server.ts
@@ -0,0 +1,19 @@
+import Fastify from "fastify";
+import { prisma } from "./src/lib/prisma";
+
+const server = Fastify({
+  logger: true,
+});
+
+const start = async () => {
+  try {
+    const port = Number(process.env.PORT) || 3000;
+    await server.listen({ port, host: "0.0.0.0" });
+    console.log(`🚀 Server ready at http://localhost:${port}`);
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
