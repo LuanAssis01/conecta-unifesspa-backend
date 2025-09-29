4b8505b41b38f86a518357441770de5a4f23906f
LuanAssis01 <assisluan863@gmail.com>
2025-09-29 11:26:17 -0300

    feat(config): add estrutura do projeto

---
+++ api/src/controller/projectController.ts
@@ -0,0 +1,8 @@
+import { FastifyRequest, FastifyReply } from 'fastify';
+import { prisma } from '../lib/prisma.ts';
+
+export const projectController = {
+  async create(request: FastifyRequest, reply: FastifyReply){
+
+  }
+}
