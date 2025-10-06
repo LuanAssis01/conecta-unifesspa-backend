2ca77183082f8116e27f0528d690377f55b922e3
LuanAssis01 <assisluan863@gmail.com>
2025-10-05 22:38:40 -0300

    feat: add project controller and routes

--- api/src/router/index.ts
+++ api/src/router/index.ts
@@ -1,7 +1,9 @@
 import { FastifyInstance } from 'fastify';
 import { userRoutes } from './userRouter.ts';
+import { projectRoutes } from './projectRouter';
 
 export async function appRoutes(app: FastifyInstance) {
   app.register(userRoutes);
+  app.register(projectRoutes);
 }
 
--- api/src/router/projectRouter.ts
+++ api/src/router/projectRouter.ts
@@ -1,12 +1,14 @@
 import { FastifyInstance } from "fastify";
 import { projectController } from "../controller/projectController";
-import { upload } from "../middlewares/upload";
+import multer from "fastify-multer";
+
+const upload = multer({ dest: "uploads/" });
 
 export async function projectRoutes(app: FastifyInstance) {
-  app.post(
-    "/projects",
-    { preHandler: upload.single("image") }, // campo "image" no form-data
-    projectController.create
-  );
+  app.post("/projects", { preHandler: upload.single("image") }, projectController.create);
+  app.get("/projects", projectController.getAll);
+  app.get("/projects/:id", projectController.getById);
+  app.put("/projects/:id", { preHandler: upload.single("image") }, projectController.update);
+  app.delete("/projects/:id", projectController.delete);
 }
 
