3e6de7e07b7a947e04e726c536c278c153bacbf1
Paulo Cesar <paulocesarpinheirodasilva0@gmail.com>
2025-10-24 15:37:48 -0300

    feat: expand impact-indicators routes with project scope

--- api/src/router/impactIndicatorsRouter.ts
+++ api/src/router/impactIndicatorsRouter.ts
@@ -2,7 +2,8 @@ import { FastifyInstance } from 'fastify';
 import { impactIndicatorsController } from '../controller/impactIndicatorsController';
 
 export async function impactIndicatorsRoutes(app: FastifyInstance) {
-    app.post('/indicators', impactIndicatorsController.create);
-    app.get('/indicators', impactIndicatorsController.getAll);
-    app.delete('/indicators/:id', impactIndicatorsController.delete);
+    app.post('/projects/:projectId/impact-indicators', impactIndicatorsController.create);
+    app.put('/projects/:projectId/impact-indicators/:indicatorId', impactIndicatorsController.update);
+    app.delete('/projects/:projectId/impact-indicators/:indicatorId', impactIndicatorsController.delete);
+    app.get('/projects/:projectId/impact-indicators', impactIndicatorsController.getByProject);
 }
