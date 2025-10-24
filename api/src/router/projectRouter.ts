cbf872b855d98f6db03e26e84ff38956233a52b7
Paulo Cesar <paulocesarpinheirodasilva0@gmail.com>
2025-10-24 15:43:04 -0300

    feat: update projects routes and handlers

--- api/src/router/projectRouter.ts
+++ api/src/router/projectRouter.ts
@@ -1,14 +1,13 @@
 import { FastifyInstance } from "fastify";
 import { projectController } from "../controller/projectController";
-import multer from "fastify-multer";
-
-const upload = multer({ dest: "uploads/" });
 
 export async function projectRoutes(app: FastifyInstance) {
-  app.post("/projects", { preHandler: upload.single("image") }, projectController.create);
+  app.post("/projects", projectController.create);
   app.get("/projects", projectController.getAll);
   app.get("/projects/:id", projectController.getById);
-  app.put("/projects/:id", { preHandler: upload.single("image") }, projectController.update);
+  app.put("/projects/:id", projectController.update);
   app.delete("/projects/:id", projectController.delete);
+  app.post('/projects/:id/proposal', projectController.updateProposal);
+  app.post('/projects/:id/image', projectController.updateImage);
+  app.patch("/projects/:id/status", projectController.updateStatus);
 }
-
