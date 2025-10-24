ae5c85dc8f749e4bb9dcfedacd49c055c2d5bed5
Paulo Cesar <paulocesarpinheirodasilva0@gmail.com>
2025-10-24 15:37:23 -0300

    feat: add GET /courses/:id

--- api/src/router/courseRouter.ts
+++ api/src/router/courseRouter.ts
@@ -4,5 +4,6 @@ import { courseController } from "../controller/courseController";
 export async function courseRoutes(app: FastifyInstance) {
     app.post('/courses', courseController.create);
     app.get('/courses', courseController.getAll);
+    app.get('/courses/:id', courseController.getById);
     app.delete('/courses/:id', courseController.delete);
 }
