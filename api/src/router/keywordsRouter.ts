9e505aaafa457a92c1a5df822bf907acb4f78799
Paulo Cesar <paulocesarpinheirodasilva0@gmail.com>
2025-10-24 15:39:43 -0300

    feat: update keywords routes with project scope

--- api/src/router/keywordsRouter.ts
+++ api/src/router/keywordsRouter.ts
@@ -2,6 +2,9 @@ import { FastifyInstance } from 'fastify';
 import { keywordsController } from '../controller/keywordsController';
 
 export async function keywordsRoutes(app: FastifyInstance) {
-    app.post('/keywords', keywordsController.create);
+    app.post('/keywords/projects/:projectId', keywordsController.create);
+    app.get('/keywords/projects/:projectId', keywordsController.getByProject);
+    app.delete('/keywords/:keywordId/projects/:projectId', keywordsController.removeFromProject)
+    app.get("/keywords/:keywordId/projects", keywordsController.getProjects);
     app.get('/keywords', keywordsController.getAll);
 }
