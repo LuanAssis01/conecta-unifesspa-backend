70213704cc7f97d10ff838d4f35e1a73430d4221
Paulo Cesar <paulocesarpinheirodasilva0@gmail.com>
2025-10-31 16:45:25 -0300

    change: add adminHook in routerCourse POST

--- api/src/router/courseRouter.ts
+++ api/src/router/courseRouter.ts
@@ -1,11 +1,13 @@
 import { FastifyInstance } from "fastify";
 import { courseController } from "../controller/courseController";
 import { isAuthenticated } from "../middleware/isAuthenticated";
+import { isAdmin } from "../middleware/adminOnly";
 
 export async function courseRoutes(app: FastifyInstance) {
   const authenticatedHook = { preHandler: [isAuthenticated] };
+  const adminHook = { preHandler: [isAdmin] };
 
-  app.post("/courses", authenticatedHook, courseController.create);
+  app.post("/courses", adminHook, courseController.create);
   app.get("/courses", courseController.getAll);
   app.get("/courses/:id", courseController.getById);
   app.delete("/courses/:id", authenticatedHook, courseController.delete);
