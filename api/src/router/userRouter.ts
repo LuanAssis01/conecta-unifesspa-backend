e4f2ff37f84cb3b85757413bc4cd12f907fd6638
Paulo Cesar <paulocesarpinheirodasilva0@gmail.com>
2025-10-31 16:55:09 -0300

    change: add adminHook in userRoutes  POST

--- api/src/router/userRouter.ts
+++ api/src/router/userRouter.ts
@@ -1,11 +1,13 @@
 import { FastifyInstance } from 'fastify';
 import { userController } from '../controller/userController';
 import { isAuthenticated } from "../middleware/isAuthenticated";
+import { isAdmin } from "../middleware/adminOnly";
 
 export function userRoutes(app: FastifyInstance) {
   const authenticatedHook = { preHandler: [isAuthenticated] };
+  const adminHook = { preHandler: [isAdmin] };
 
-  app.post("/user", userController.create);
+  app.post("/user", adminHook, userController.create);
   app.post("/login", userController.login);
   app.put("/profile", authenticatedHook, userController.update);
 }
