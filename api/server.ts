6cc9fdc6aa937036903b449d65374bd1b3b04010
LuanAssis01 <assisluan863@gmail.com>
2025-12-13 00:49:57 -0300

    fix: ajusted

--- api/server.ts
+++ api/server.ts
@@ -45,7 +45,6 @@ const start = async () => {
   try {
     const port = Number(process.env.PORT) || 3333
     await server.listen({ port, host: '0.0.0.0' })
-    console.log(`🚀 Server ready at http://localhost:${port}`)
   } catch (err) {
     server.log.error(err)
     process.exit(1)
