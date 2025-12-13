e3efe4e8996552de5fecf41ef584f60c8418e511
LuanAssis01 <assisluan863@gmail.com>
2025-12-13 11:57:41 -0300

    fix: ajusted 12

---
+++ api/server.ts
@@ -0,0 +1,31 @@
+import Fastify from 'fastify';
+import { appRoutes } from './src/router/index';
+import cors from "@fastify/cors";
+import multipart from '@fastify/multipart';
+import { prisma } from './src/lib/prisma';
+
+const server = Fastify({
+  logger: true,
+});
+
+server.register(cors, {
+  origin: "*",
+});
+
+server.register(multipart, {
+  limits: { fileSize: 10 * 1024 * 1024 }, // até 10 MB
+});
+server.register(appRoutes);
+
+const start = async () => {
+  try {
+    const port = Number(process.env.PORT) || 3333;
+    await server.listen({ port, host: "0.0.0.0" });
+    console.log(`🚀 Server ready at http://localhost:${port}`);
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
